74c24aa3e6496ea4c58f9fb365bb88b36e255038
arman <>
2020-07-05 19:15:19 -0700

    ui loads

--- packs/behaviors/scripts/client/client.js
+++ packs/behaviors/scripts/client/client.js
@@ -1,7 +1,7 @@
 const clientSystem = client.registerSystem(0, 0);
 
 // Setup which events to listen for
-clientSystem.initialize = () =>
+clientSystem.initialize = function ()
 {
 	// const eventDataDefaults = {narf: false};
 	// clientSystem.registerEventData("gettingstarted:pinky", eventDataDefaults);
@@ -12,8 +12,25 @@ clientSystem.initialize = () =>
 	scriptLoggerConfig.data.log_warnings = true;
 	clientSystem
 		.broadcastEvent("minecraft:script_logger_config", scriptLoggerConfig);
+
+	this.listenForEvent("minecraft:client_entered_world", (eventData) =>
+		this.onClientEnteredWorld(eventData));
 };
 
 clientSystem.update = () =>
 {
+};
+
+clientSystem.onClientEnteredWorld = function (eventData)
+ {
+    // Client has entered the world, show the starting screen
+    let loadEventData = this.createEventData("minecraft:load_ui");
+    loadEventData.data.path = "settings.html";
+    loadEventData.data.options.is_showing_menu = false;
+    loadEventData.data.options.absorbs_input = true;
+    clientSystem.broadcastEvent("minecraft:load_ui", loadEventData);
+
+    // Notify the server script that the player has finished loading in.
+  //   let clientEnteredEventData = this.createEventData("rpg_game:client_entered_world");
+	// aClientSystem.broadcastEvent("rpg_game:client_entered_world", clientEnteredEventData);
 };
